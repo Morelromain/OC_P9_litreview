b6a0e9d68e713e5501c037582df1c6b0506afcfd
Romain Morel <morelromain86@gmail.com>
2021-03-02 11:15:43 +0100

    Create 'subscription' system

--- utilisateur/forms.py
+++ utilisateur/forms.py
@@ -1,9 +1,15 @@
 from django.forms import forms, ModelForm
 
 from utilisateur.models import User
+from utilisateur.models import UserFollows
 
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+class UserFollowsForm(ModelForm):
+    class Meta:
+        model = UserFollows
+        fields = ['followed_user']
--- utilisateur/urls.py
+++ utilisateur/urls.py
@@ -7,13 +7,9 @@ from OC_P9 import settings
 
 
 urlpatterns = [
-    
     path('', views.connection, name="connection"),
     path('register', views.create_user, name="register"),
-    path('test', views.test, name="test"),
-    
+    path('subscription', views.subscription, name="subscription"),
     path('accounts/', include('django.contrib.auth.urls')),   
-
-
-
+    path('delete_subs/<int:id_subs>', views.delete_subs, name="delete_subs"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
