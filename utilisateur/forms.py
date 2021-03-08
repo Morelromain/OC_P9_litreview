b66c4de255e601f1fbfded735dcf39406fbd1751
Romain Morel <morelromain86@gmail.com>
2021-03-08 15:24:09 +0100

    finalisation

--- utilisateur/forms.py
+++ utilisateur/forms.py
@@ -9,19 +9,6 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'first_name']
-        widgets = {
-            'username': forms.TextInput(attrs={'size': 134}),
-            'password': forms.TextInput(attrs={'size': 134}),
-            'first_name': forms.TextInput(attrs={'size': 134})
-            }
-        labels = {
-            'username': 'Nom',
-            'password': 'Mot de passe',
-            'first_name': 'Confirmer mot de passe',
-        }
-        help_texts = {
-            'username': None,
-        }
 
 
 class UserFollowsForm(ModelForm):
